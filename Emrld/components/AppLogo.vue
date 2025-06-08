f8bc423fd69fa5b380e89f22432d691200bf5772
o004 <88547837+kxm004@users.noreply.github.com>
2025-06-09 02:50:48 +0300

    Delete Emrld directory

--- Emrld/components/AppLogo.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="flex items-center animate-slide-up">
-    <div class="w-8 h-8  bg-primary-500 flex items-center justify-center mr-2">
-      <!-- <span class="text-white font-bold text-lg">E</span> -->
-    </div>
-    <span class="font-bold text-xl text-neutral-900 dark:text-white">Emrld</span>
-  </div>
-</template>
--- Emrld/layouts/auth.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="min-h-screen flex flex-col">
-    <main class="bg-zinc-900 flex-1 flex flex-col items-center justify-center">
-      <div class="w-full max-w-md">
-        <slot />
-      </div>
-    </main>
-  </div>
-</template>
